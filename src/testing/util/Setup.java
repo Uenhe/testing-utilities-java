ec22e1da580907f250de9d8f12bb94747d521969
KochiyaUenehaaa <119563256+Uenhe@users.noreply.github.com>
2025-02-04 23:50:29 +0800

    a

--- src/testing/util/Setup.java
+++ src/testing/util/Setup.java
@@ -59,57 +59,6 @@ public class Setup{
             if(timeControlEnabled()) table.add(yoinkTimeSlider());
             table.table(Tex.pane, Death::seppuku);
         }, () -> true);
-
-        Label pos = ((Table)ui.hudGroup.find("minimap/position")).find("position");
-        pos.setText(() -> {
-            String playerPos = "";
-            if(settings.getBool("position")){
-                playerPos = player.tileX() + ", " + player.tileY() + "\n";
-                if(settings.getBool("tu-wu-coords", true)){
-                    playerPos += "[accent]" + fix(player.x) + ", " + fix(player.y) + "\n";
-                }
-            }
-
-            int tx = World.toTile(Core.input.mouseWorldX()),
-                ty = World.toTile(Core.input.mouseWorldY());
-
-            String cursorPos = "";
-            if(settings.getBool("mouseposition")){
-                cursorPos = "[lightgray]" + tx + ", " + ty + "\n";
-                if(settings.getBool("tu-wu-coords", true)){
-                    cursorPos += "[#d4816b]" + fix(Core.input.mouseWorldX()) + ", " + fix(Core.input.mouseWorldY()) + "\n";
-                }
-
-                if(settings.getBool("tu-tile-info", false)){
-                    Tile tile = world.tile(tx, ty);
-                    cursorPos += "[#a9d8ff]";
-                    if(tile == null){
-                        cursorPos += "-----";
-                    }else{
-                        cursorPos += tile.floor().localizedName
-                            + " | " + tile.overlay().localizedName
-                            + " | " + tile.block().localizedName
-                            + " | data = ";
-                        StringBuilder data = new StringBuilder();
-                        for(int i = 7; i >= 0; i--){
-                            data.append((tile.data & (1 << i)) != 0 ? '1' : '0');
-                        }
-                        cursorPos += data;
-                    }
-                }
-            }
-
-            return playerPos + cursorPos;
-        });
-
-        terrainFrag = new TerrainPainterFragment();
-        Core.app.post(() -> terrainFrag.build(ui.hudGroup)); //Wait for BLUI to set up.
-
-        Events.on(WorldLoadEvent.class, e -> {
-            if(posLabelAligned) return;
-            pos.setAlignment(Align.right, Align.right);
-            posLabelAligned = true;
-        });
     }
 
     private static Table yoinkTimeSlider(){
